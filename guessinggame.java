109ec8ff6125d7dcda7b51db0557ad3cfdbe7a7c
jghoover <30665181+jghoover@users.noreply.github.com>
2017-08-21 09:31:53 -0500

    Added template

---
+++ guessinggame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; //need scanner class to read user input
+
+class guessinggame
+{
+  public static void main(String[] args)
+  {
+    //code goes inside main function
+  }
+}
