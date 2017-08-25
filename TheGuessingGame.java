102684ae74bf641af095796ff3ce39127a8d6eba
Michael Czoer <31040231+michaelczoer@users.noreply.github.com>
2017-08-25 10:01:47 -0500

    Add files via upload

--- TheGuessingGame.java
+++ TheGuessingGame.java
@@ -2,48 +2,78 @@ package Projects;
 
 import java.util.Scanner;
 
-public class TheGuessingGame {
+public class TheGuessingGame 
+{
 
 	
 
 	public static void main(String[] args) 
+	
 	{
-		
 		Scanner cin = new Scanner(System.in);
 		
-		int use = cin.nextInt();
+		int ben=1;	//Ben is used later on to loop
 		
-		boolean notCod=true;
+		while(ben==1)
+			
+			
+		{			
 		
-		System.out.println("Im thinking of a number between 1 and " +use+ ".");
-		
-		int random = (int)(1+use*Math.random());
-		
-		while(notCod==true)
-		{
-		
-		int can1 = cin.nextInt();
-		
-	if(can1<random)
-	{
-		
-		System.out.println("Too low!");
+			System.out.println("Pick a number between negative infinity and infinity.");
+			
+			int use = cin.nextInt(); //Given upper bound number
 				
-	}
-	else if(can1==random)
-	{
+			boolean notCod=true;
 		
-		System.out.print("You win!");
-		notCod=false;
+			System.out.println("Im thinking of a number between 1 and " +use+ ".");
 		
-	}
-	else
-	{
+			int random = (int)(1+use*Math.random());	//Random number generator between 1 and given value
 		
-		System.out.println("Too high!");
+			while(notCod==true)	//Check later in code for notCod explanation
+			{
 		
+				int can1 = cin.nextInt();	//Users guess
+		
+				if(can1<random)
+				{
+		
+					System.out.println("Too low!");
+				
+				}
+				else if(can1==random)
+				{
+		
+					System.out.print("You win!");
+		
+					notCod=false;	//It will keep looping the guessing until they get it right then will move on
+		
+				}
+				else
+				{
+		
+					System.out.println("Too high!");
+		
+				}
+			}
+		
+			System.out.println("");
+		
+			System.out.println("Would you like to play again?");
+		
+			System.out.println("1 for yes, 0 for no");
+		
+			ben = cin.nextInt();	//ben is the value that determines if the whole code will be repeated
+		
+			if(ben==0)
+			{
+			
+				System.out.println("OK hope you enjoyed the game!");
+			
+			}
+			
+		}
+		
+		cin.close();
 	}
-	cin.close();
-	}
-}
+		
 }
